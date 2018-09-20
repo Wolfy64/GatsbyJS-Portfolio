9c79946c6f09f4ae2c4b8595018e5523f6329cb4
David <ledavid64@gmail.com>
2018-09-20 15:16:09 -0700

    Add components folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,10 @@
-import React from "react"
+import React from 'react'
+import Layout from '../components/layout'
+import Navbar from '../components/navbar/navbar'
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <Layout>
+    <Navbar />
+    Hello world!
+  </Layout>
+)
