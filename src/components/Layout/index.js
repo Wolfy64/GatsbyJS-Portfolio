5bbbf75c33cc092e453eeb5187c608448e1757a9
David <ledavid64@gmail.com>
2018-09-28 17:14:22 -0700

    Add CCS Grid

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,13 +1,24 @@
 import React from 'react'
+import styled from 'styled-components'
 import Head from './Head'
 import Navbar from './Navbar'
 import Footer from './Footer'
 
+const Layout = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: repeat(3 1fr);
+  grid-template-areas:
+    'Navbar'
+    'Main'
+    'Footer';
+`
+
 export default ({ children }) => (
-  <React.Fragment>
+  <Layout>
     <Head />
     <Navbar />
     {children}
     <Footer />
-  </React.Fragment>
+  </Layout>
 )
