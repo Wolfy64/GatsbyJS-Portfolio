4dbe2e3a58f87db8341bbe0275e4bb16e3517af1
David <ledavid64@gmail.com>
2018-09-28 17:17:47 -0700

    Add Plugin Typografy

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-styled-components`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
 }
---
+++ src/utils/typography.js
@@ -0,0 +1,18 @@
+import Typography from 'typography'
+
+const typography = new Typography({
+  baseFontSize: '18px',
+  baseLineHeight: 1.666,
+  headerFontFamily: [
+    'Varela Round',
+    'Avenir Next',
+    'Helvetica Neue',
+    'Segoe UI',
+    'Helvetica',
+    'Arial',
+    'sans-serif',
+  ],
+  bodyFontFamily: ['Playfair Display', 'Georgia', 'serif'],
+})
+
+export default typography
