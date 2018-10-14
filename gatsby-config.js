49babb5d7c9f014c6b251d6b676b75a395d3ea9f
David De Wulf <david@Davids-MacBook-Pro.local>
2018-10-14 12:28:25 -0700

    Add Netlify CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-netlify-cms`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {
