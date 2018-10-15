f1d85d54b5cea8254025684aeeb3093ded831a3c
David De Wulf <david@Davids-MacBook-Pro.local>
2018-10-15 15:16:31 -0700

    Add npm plugin to read Markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,10 +3,18 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-netlify-cms`,
+    `gatsby-transformer-remark`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/path/to/markdown/files`,
+        name: 'markdown-pages',
       },
     },
   ],
