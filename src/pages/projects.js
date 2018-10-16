53ac6aba2434dd13ea06b75237aec0d579efab88
David De Wulf <david@Davids-MBP.attlocal.net>
2018-10-15 17:49:42 -0700

    Add ProjectCard Component

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,3 +1,37 @@
 import React from 'react'
+import { graphql } from 'gatsby'
+
 import Layout from '../components/layout'
-export default () => <Layout>Projects</Layout>
+import ProjectCard from '../components/ProjectCard'
+
+const ProjectsList = ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) => {
+  const Posts = edges
+    .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
+    .map(edge => <ProjectCard key={edge.node.id} post={edge.node} />)
+
+  return <Layout>{Posts}</Layout>
+}
+
+export default ProjectsList
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            path
+            title
+          }
+        }
+      }
+    }
+  }
+`
