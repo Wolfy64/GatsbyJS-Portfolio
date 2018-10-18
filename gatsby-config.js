3a3fee363c194f9d68500ffe39c79c617780dfaa
David De Wulf <david@Davids-MBP.attlocal.net>
2018-10-17 18:10:40 -0700

    Add Gatsby-plugin image

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,8 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-netlify-cms`,
-    `gatsby-transformer-remark`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {
@@ -16,8 +17,32 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/markdown/projects`,
         name: 'markdown-pages',
+        path: `${__dirname}/src/markdown/projects`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 100,
+              wrapperStyle: `margin-bottom:10px; background: red;`,
+            },
+          },
+        ],
       },
     },
   ],
