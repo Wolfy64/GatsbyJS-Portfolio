263e3788d73a06d10c47169ba1e687adb941f9ed
David De Wulf <david@Davids-MBP.attlocal.net>
2018-10-19 16:22:49 -0700

    Refactore Terminal Component

--- src/components/Terminal.js
+++ src/components/Terminal.js
@@ -1,49 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import Emoji from './UI/Emoji'
-
-const Terminal = styled.div`
-  max-width: 600px;
-  margin: auto;
-  margin-top: 3rem;
-  text-align: initial;
-`
-const Topbar = styled.div`
-  border-radius: 5px 5px 0 0;
-  background: #ebebeb;
-  display: flex;
-  height: 30px;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-`
-const Circle = styled.div`
-  height: 10px;
-  width: 10px;
-  display: inline-block;
-  border-radius: 50%;
-  margin-left: 5px;
-  background-color: ${props => props.color};
-  position: absolute;
-  left: ${props => props.margin};
-`
-const Window = styled.div`
-  margin: 0px;
-  padding: 10px;
-  width: 100%;
-  min-height: 300px;
-  display: inline-block;
-  background: ${props => props.bgColor};
-  border-radius: 0 0 2px 2px;
-  color: ${props => props.textColor};
-`
-const Code = styled.code`
-  font-family: 'Raleway', 'sans-serif';
-  word-spacing: ${props => (props.skills ? '15px' : null)};
-  color: ${props => props.skills};
-`
-
 const SKILLS = [
   'JavaScript-ReactJS',
   'HTML5-CSS3',
@@ -54,25 +11,64 @@ const SKILLS = [
   'Git-GitHub',
 ]
 
-export default () => (
-  <div className="container">
-    <Terminal>
-      <Topbar>
-        <Circle color="#fe5f55;" margin="5px" />
-        <Circle color="#ffbd2e" margin="20px" />
-        <Circle color="#29ca3f" margin="35px" />
-        <Emoji symbol="👨🏻‍💻 David - Skills" />
-      </Topbar>
-      <Window bgColor="#002c36" textColor="#f2f2f2">
-        <Code>
-          Last login: {new Date().toString().slice(0, 15)}
-          <br />
-        </Code>
+const Terminal = styled.div`
+  max-width: 500px;
+  margin: auto;
+  box-shadow: 0 6px 34px 0px hsl(230, 80%, 90%);
+  border-radius: 5px;
+`
 
-        <Code skills="#79b6f2">
-          {SKILLS.reduce((output, skill) => (output += `${skill}\n`), '')}
-        </Code>
-      </Window>
-    </Terminal>
-  </div>
+const Topbar = styled.div`
+  background-color: hsl(230, 80%, 70%);
+  border-radius: 5px 5px 0px 0px;
+  display: flex;
+  height: 30px;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+`
+
+const Circle = styled.div`
+  height: 10px;
+  width: 10px;
+  display: inline-block;
+  border-radius: 50%;
+  margin-left: 5px;
+  background-color: ${props => props.color || 'white'};
+  position: absolute;
+  left: ${props => props.margin};
+`
+
+const Window = styled.div`
+  margin: 0px;
+  padding: 10px;
+  width: 100%;
+  min-height: 300px;
+  display: inline-block;
+  background: white;
+  color: hsla(0, 0%, 0%, 0.7);
+  border-radius: 0px 0px 5px 5px;
+`
+
+const Code = styled.code`
+  display: block;
+  font-family: 'Lato';
+  word-spacing: ${props => (props.skills ? '15px' : null)};
+  color: ${props => props.skills};
+`
+
+export default () => (
+  <Terminal>
+    <Topbar>
+      <Circle margin="5px" color="hsl(230, 80%, 90%)" />
+      <Circle margin="20px" />
+      <Circle margin="35px" />
+    </Topbar>
+    <Window>
+      <Code children={`Last login: ${new Date().toString().slice(0, 15)}`} />
+      <Code skills="hsl(230, 80%, 70%)">
+        {SKILLS.reduce((output, skill) => (output += `${skill}\n`), '')}
+      </Code>
+    </Window>
+  </Terminal>
 )
