d40d498203770ed9fd562031a13b4f649c92dda5
David De Wulf <david@Davids-MBP.attlocal.net>
2018-10-19 17:45:57 -0700

    Refactor Card Component

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,66 +3,53 @@ import { Link } from 'gatsby'
 import styled from 'styled-components'
 
 const Card = styled.div`
-  display: grid;
+  display: inline-block;
   background: white;
   border-radius: 5px;
   max-width: 270px;
-  box-shadow: 0 6px 34px rgba(83, 40, 255, 0.15);
-  transition: linear 300ms;
-
-  h1,
-  p {
-    margin: 0;
+  box-shadow: 0 6px 34px 0px hsl(230, 80%, 90%);
+  transition: all ease-out 300ms;
+  :hover {
+    transform: scale(1.05);
+    transition: all ease-out 300ms;
+    box-shadow: 0 6px 34px 0px hsl(230, 80%, 70%);
   }
 
-  header,
-  article,
-  footer {
-    margin: 1em;
-  }
+  hr{ margin: 0 1em; },
+`
+const ImgCard = styled.img`
+  border-radius: 5px 5px 0px 0px;
+  margin: 0;
+`
 
-  a {
-  }
-  &&:hover {
-    transform: scale(1.1);
-    transition: linear 300ms;
-    box-shadow: 0 6px 34px rgba(83, 40, 255, 0.41);
-  }
-  .cover {
-    border-radius: 5px 5px 0px 0px;
-    margin: 0;
-  }
-  .title {
-    font-size: 1em;
-  }
-  .summary {
-    font-size: 0.8em;
-  }
-  .tag {
-    text-transform: uppercase;
-    font-size: 0.7em;
-    font-weight: bolder;
-  }
+const TitleCard = styled.h1`
+  font-size: 1em;
+  margin: 1rem;
+`
+
+const SummaryCard = styled.p`
+  font-size: 0.8em;
+  margin: 1rem;
+`
+
+const TagCard = styled.p`
+  text-transform: uppercase;
+  font-size: 0.7em;
+  font-weight: bolder;
+  border-radius: 0 0 5px 5px;
+  margin: 1rem;
 `
 
 const PostCard = ({ project }) => (
-  <Card>
-    <Link className="link" to={project.frontmatter.path}>
-      <section>
-        <img className="cover" src={project.frontmatter.cover} alt="Project" />
-        <header>
-          <h1 className="title">{project.frontmatter.title}</h1>
-        </header>
-        <article>
-          <p className="summary">{project.frontmatter.summary}</p>
-        </article>
-        <hr />
-        <footer>
-          <p className="tag">{project.frontmatter.tag}</p>
-        </footer>
-      </section>
-    </Link>
-  </Card>
+  <Link className="link" to={project.frontmatter.path}>
+    <Card>
+      <ImgCard src={project.frontmatter.cover} alt="Project" />
+      <TitleCard children={project.frontmatter.title} />
+      <SummaryCard children={project.frontmatter.summary} />
+      <hr />
+      <TagCard children={project.frontmatter.tag} />
+    </Card>
+  </Link>
 )
 
 export default PostCard
