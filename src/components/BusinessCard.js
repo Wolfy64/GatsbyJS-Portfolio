15941196f0b0447f7181d35ddfdfefefe55b56e3
David De Wulf <contact.dewulf@gmail.com>
2018-10-23 10:00:06 -0700

    Add Picture Profile

--- src/components/BusinessCard.js
+++ src/components/BusinessCard.js
@@ -1,58 +1,60 @@
 import React from 'react'
 import styled from 'styled-components'
+import { StaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
 
-import profil from '../images/profil.jpg'
 import SocialNetwork from '../components/SocialNetwork'
 
-const Card = styled.div`
+const Card = styled.aside`
   align-self: center;
   width: 300px;
   margin: 2em 0;
-  /* transition: all ease-out 300ms;
-  :hover {
-    transform: scale(1.05);
-    transition: all ease-out 300ms;
-    article {
-      box-shadow: 0 6px 34px 0px hsl(230, 80%, 70%);
-      transition: all ease-out 300ms;
-    }
-  } */
   @media (max-width: 850px) {
     margin: 2em auto;
   }
-`
 
-const Profil = styled.figure`
-  img {
-    border-radius: 50%;
-    display: block;
-    width: 100px;
-    height: 100px;
-    margin: auto;
-    margin-bottom: -80px;
-    box-shadow: 0px -3px 5px 0px hsl(230, 80%, 90%);
-  }
-`
-
-const Summary = styled.article`
-  border-radius: 5px;
-  box-shadow: 0 6px 34px 0px hsl(230, 80%, 90%);
-  height: 200px;
   p {
     text-align: center;
     padding-top: 3em;
     margin: 1em;
   }
+
+  .gatsby-image-wrapper {
+    margin: auto auto -80px auto;
+  }
+`
+
+const Summary = styled.div`
+  border-radius: 5px;
+  box-shadow: 0 6px 34px 0px hsl(230, 80%, 90%);
+  height: 200px;
 `
 
 export default () => (
-  <Card>
-    <Profil>
-      <img src={profil} alt="David De Wulf" />
-    </Profil>
-    <Summary>
-      <p>Looking for a Front End position</p>
-      <SocialNetwork />
-    </Summary>
-  </Card>
+  <StaticQuery
+    query={graphql`
+      query {
+        imageOne: file(relativePath: { eq: "david.png" }) {
+          childImageSharp {
+            fixed(width: 150, height: 150) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <Card>
+        <Img
+          alt="David De Wulf"
+          fixed={data.imageOne.childImageSharp.fixed}
+          style={{ display: 'inherit' }}
+        />
+        <Summary>
+          <p>Looking for a Front End position</p>
+          <SocialNetwork />
+        </Summary>
+      </Card>
+    )}
+  />
 )
