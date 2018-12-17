90eda6964204243ec912339f5b114abe88f64151
David De Wulf <contact.dewulf@gmail.com>
2018-12-17 12:01:39 -0800

    Update Text

--- src/components/About.js
+++ src/components/About.js
@@ -34,19 +34,51 @@ const OpenClassrooms = (
   <A href="https://openclassrooms.com/en/for-business" name="OpenClassrooms" />
 )
 
+const WesBos = <A href="https://wesbos.com/" name="WesBos" />
+const Udemy = (
+  <A
+    href="https://media.licdn.com/media-proxy/ext?w=800&h=800&f=n&hash=bO172KEfJPLbcxTvXq89ISNc8Fw%3D&ora=1%2CaFBCTXdkRmpGL2lvQUFBPQ%2CxAVta5g-0R6jnhodx1Ey9KGTqAGj6E5DQJHUA3L0CHH05IbfPWjhKM7ZLbWhoEBEeykHjQBkKbm1SGKyRI7tetvudNgkjcbldZP5agYUbhl4j3lK6w"
+    name="Udemy"
+  />
+)
+
+const FreeCodeCamp = (
+  <A
+    href="https://www.freecodecamp.org/certification/wolfy64/responsive-web-design"
+    name="FreeCodeCamp"
+  />
+)
+const C0d3 = <A href="https://c0d3.com/" name="c0d3.com" />
+const Here = <A href="mailto:contact.dewulf@gmail.com" name="HERE" />
+
 export default () => (
   <About>
     <h1>About Me</h1>
     <p>
-      I’m a <mark>web developer</mark>, who changed careers to do a cool job.
-      <br />I have just completed a{' '}
-      <mark>Bachelor's Degree in Web Development</mark> with {OpenClassrooms}.
+      I have always been passionate about coding, which is why after 15 years as
+      an optician I decided to seize the opportunity to turn my passion into a
+      career through a <mark>Computing Sciences Bachelors.</mark>
       <br />
-      Throughout this course I have learned how to effectively work
-      independently and the importance of self-motivation, allowing me to be
-      efficient as well as highly adaptable, one of the greatest assets for a
-      profile in today’s working society.
-      <br /> I would like to work as a <mark>Javascript web developer.</mark>
+      Throughout, hackathons, Open Source projects and online certifications (
+      {OpenClassrooms}, {WesBos}, {Udemy}, {FreeCodeCamp} …etc) I have since
+      then been able to build my own projects, contribute to team projects and
+      join ever-expanding developer communities like {C0d3} without ever
+      stopping to learn.
+    </p>
+    <p>
+      Always looking to improve my set of skills, I am genuinely curious,
+      particularly meticulous and have an eye for the detail which is why I am
+      committed to building a clean, reusable, and easily maintainable as
+      well-commented code.
+    </p>
+    <p>
+      I am currently specialized in Front-end development and actively working
+      on projects to building a <mark>FullStack App with ReactJS</mark> on the
+      front-end on my spare time.
+    </p>
+    <p>
+      Interested in collaborating? Please reach out {Here}. I look forward to
+      speaking with you soon, be it hiring or building a website remotely.
     </p>
   </About>
 )
