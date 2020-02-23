d4c22dd59eb2e506c5da024037c557a8e02e0f11
David De Wulf <ledavid64@gmail.com>
2020-02-22 23:15:35 -0800

    Updated text

--- src/components/About.js
+++ src/components/About.js
@@ -30,57 +30,41 @@ const About = styled.article`
   }
 `
 
-const OpenClassrooms = (
-  <A href="https://openclassrooms.com/en/for-business" name="OpenClassrooms" />
-)
-
-const WesBos = <A href="https://wesbos.com/" name="WesBos" />
-const Udemy = (
+const c0d3 = <A href="https://c0d3.com/" name="c0d3.com" />
+const HwyHaul = <A href="https://hwyhaul.com" name="Hwy Haul" />
+const openEmail = (
   <A
-    href="https://media.licdn.com/media-proxy/ext?w=800&h=800&f=n&hash=bO172KEfJPLbcxTvXq89ISNc8Fw%3D&ora=1%2CaFBCTXdkRmpGL2lvQUFBPQ%2CxAVta5g-0R6jnhodx1Ey9KGTqAGj6E5DQJHUA3L0CHH05IbfPWjhKM7ZLbWhoEBEeykHjQBkKbm1SGKyRI7tetvudNgkjcbldZP5agYUbhl4j3lK6w"
-    name="Udemy"
+    href="mailto:contact.dewulf@gmail.com"
+    name="So, interested in collaborating?"
   />
 )
-
-const FreeCodeCamp = (
-  <A
-    href="https://www.freecodecamp.org/certification/wolfy64/responsive-web-design"
-    name="FreeCodeCamp"
-  />
-)
-const C0d3 = <A href="https://c0d3.com/" name="c0d3.com" />
-const Here = <A href="mailto:contact.dewulf@gmail.com" name="HERE" />
 
 export default () => (
   <About>
     <h1>About Me</h1>
     <p>
       I have always been passionate about coding, which is why after 15 years as
-      an optician I have decided to seize the opportunity to turn my passion
-      into a career through a <mark>Computing Sciences Bachelors.</mark>
+      an Optometrist I have decided to seize the opportunity to turn my passion
+      into a career and got a <mark>Computing Sciences Bachelors.</mark>
       <br />
-      Throughout hackathons, Open Source projects and online certifications (
-      {OpenClassrooms}, {WesBos}, {Udemy}, {FreeCodeCamp} …etc).
-      <br />I have since then been able to build my own projects, contribute to
-      team projects and join ever-expanding developer communities like {
-        C0d3
-      }{' '}
-      without ever stopping to learn.
-    </p>
-    <p>
+      Throughout hackathons, open source projects and online certifications I
+      was then able to contribute to team projects and join ever-expanding
+      developer communities like {c0d3} without ever stopping to learn.
+      <br />
       Always looking to improve my set of skills, I am genuinely curious,
       particularly meticulous and have an eye for detail which is why I am
       committed to building a clean, reusable, and easily maintainable
       well-commented code.
     </p>
     <p>
-      I am currently specialized in Front-end development and actively working
-      on projects including building a <mark>FullStack App with ReactJS</mark>{' '}
-      on the front-end on my spare time.
-    </p>
-    <p>
-      Interested in collaborating? Please reach out {Here}.<br />I look forward
-      to speaking with you soon, be it hiring or building a website remotely.
+      I am currently working as a <mark>Front-end Developer</mark> at {HwyHaul}{' '}
+      startup and on my spare time actively working on open source projects for{' '}
+      {c0d3}.
+      <br />
+      I look forward to speaking with you soon, be it hiring or building a
+      website remotely.
+      <br />
+      {openEmail}
     </p>
   </About>
 )
