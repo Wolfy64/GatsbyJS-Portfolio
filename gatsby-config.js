9fada41006dcea69911b04361d928a9eec0d5dca
David De Wulf <ledavid64@gmail.com>
2021-03-04 11:58:00 -0800

    Setup Typrscript

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,36 +1,6 @@
-module.exports = {
-  siteMetadata: {
-    title: "gatsby-v3",
-  },
-  plugins: [
-    "gatsby-plugin-theme-ui",
-    "gatsby-plugin-gatsby-cloud",
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
-    },
-    "gatsby-plugin-mdx",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
-    },
-  ],
-};
+/**
+ * To have full Typescript support with Gatsby
+ * https://github.com/progressive-dev/gatsby-typescript-final.git
+ */
+require('ts-node').register() // This will register the TypeScript compiler
+module.exports = require('./src/gatsby/config') // This will load our Typescript application
---
+++ src/gatsby/config.ts
@@ -0,0 +1,36 @@
+export default {
+  siteMetadata: {
+    title: "gatsby-v3",
+  },
+  plugins: [
+    "gatsby-plugin-theme-ui",
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
