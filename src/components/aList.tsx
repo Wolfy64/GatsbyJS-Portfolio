e2dc6b508b88085930c6ad9d2d97971b22f8e4cf
David De Wulf <ledavid64@gmail.com>
2021-03-29 22:21:51 -0700

    Add list component

---
+++ src/components/aList.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react'
+import styled from 'styled-components'
+
+interface Props {
+  children: React.ReactNode
+  [props: string]: unknown
+}
+
+const Li = styled.li`
+    display: flex;
+    margin: 8px;
+    :before {
+      content: '\f120';
+      font-size: 0.7em;
+      padding-right: 4px;
+      font-family: 'FontAwesome';
+      color: ${({ theme }) => theme.colors.primary};
+    }
+`
+
+const Alist = (props: Props) => <Li {...props} />
+
+export default Alist
