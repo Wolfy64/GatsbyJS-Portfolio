fad227c34e809204ef3ece24a5b4c28f772e27ca
David De Wulf <ledavid64@gmail.com>
2021-04-02 17:53:34 -0700

    Add component memo

---
+++ src/components/memo.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react'
+import styled from 'styled-components'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
+import { MDXProvider } from '@mdx-js/react'
+import Layout from './layout'
+import Alink from './aLink'
+import CodeBlock from './codeBlock'
+import { Node } from '../gatsby/node'
+
+interface Props {
+  children: React.ReactNode
+  pageContext: Omit<Node, 'slug'>
+}
+
+const Container = styled.div`
+  img {
+    display: inline;
+    border-radius: ${({ theme }) => theme.radii};
+  }
+
+  .center {
+    display: block;
+    margin: 16px auto;
+  }
+`
+
+const Memo = ({ pageContext: { body } }: Props) => {
+  return (
+    <Layout>
+      <Container>
+        <MDXProvider
+          components={{
+            a: Alink,
+            pre: CodeBlock,
+          }}
+        >
+          <MDXRenderer children={body} />
+        </MDXProvider>
+      </Container>
+    </Layout>
+  )
+}
+
+export default Memo
